cebaa9589b34b941db1b958440b6a2bb7ff39601
Murray Logan <i.obesulus@gmail.com>
2019-03-27 16:11:32 +1000

    Standardized residuals

--- interactions.R
+++ interactions.R
@@ -10,3 +10,17 @@ for (i in 1:(length(terms)-1)) {
   }
 }
 abalone.int %>% arrange(-H.stat)
+
+
+## Standardized Residuals for 
+coefs = as.matrix(peake.glmP)[,1:2]
+coefs = as.matrix(peake.glmP)[,c('(Intercept)','log(AREA)')]
+Xmat = model.matrix(~log(AREA), data=peake)
+fit = exp(coefs %*% t(Xmat))
+#Pearson residuals
+resid = sweep(fit, 2, peake$INDIV, "-")
+# operate on rows
+sd = apply(fit,2,sqrt)
+resid=resid/sd
+ggplot(data=NULL) +
+  geom_point(aes(y=colMeans(resid), x=fitted(peake.glmP)))
